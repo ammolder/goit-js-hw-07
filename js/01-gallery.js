e7e34359c79c25b505ed3538f88e11a8dcb23acb
ammolder <98980721+ammolder@users.noreply.github.com>
2022-08-15 13:56:13 +0200

    add library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,37 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-import * as basicLightbox from "./01-gallery.js";
 
-// const instance = basicLightbox.create(`
-//     <img src=${onLinkContainerClick} width="800" height="600">
-// `);
-// instance.show();
-
-console.log(galleryItems);
 // 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 const divEl = document.querySelector(".gallery");
-const ulEl = document.createElement("ul");
-ulEl.classList.add("gallery__list");
-divEl.prepend(ulEl);
-
-console.log(divEl);
 
 const addMarketing = galleryItems
   .map(
     (galleryItems) =>
-      `<li class="gallery__item"><img class="gallery__image" src = ${galleryItems.preview} alt='${galleryItems.description}'><a class="gallery__link" src=${galleryItems.original}></a></li>`
+      `<a class="gallery__link" href=${galleryItems.original}>
+    <img
+      class="gallery__image"
+      src=${galleryItems.preview}
+      data-source=${galleryItems.original}
+      alt="${galleryItems.description}"
+    />
+  </a>`
   )
   .join("");
-ulEl.insertAdjacentHTML("afterbegin", addMarketing);
+divEl.insertAdjacentHTML("afterbegin", addMarketing);
 
 // 2. Реалізація делегування на div.gallery і отримання url великого зображення.
 divEl.addEventListener("click", onLinkContainerClick);
 
 function onLinkContainerClick(e) {
+  e.preventDefault();
+
   if (e.target.nodeName !== "IMG") {
     return;
   }
 
-  const linkImage = e.target.nextSibling.attributes[1].value;
-  console.log("linkImage :", linkImage);
+  const instance = basicLightbox.create(`
+    <img src=${e.target.dataset.source}>
+`);
 
-  return linkImage;
+  instance.show();
 }
-
-// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-
-//
-
-// instance.show();
-// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divEl = document.querySelector(".gallery");
+
+const addMarketing = galleryItems
+  .map(
+    (galleryItems) =>
+      `<a class="gallery__item" href=${galleryItems.original}>
+  <img class="gallery__image" src=${galleryItems.preview} alt="${galleryItems.description}" />
+</a>`
+  )
+  .join("");
+divEl.insertAdjacentHTML("afterbegin", addMarketing);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+});
